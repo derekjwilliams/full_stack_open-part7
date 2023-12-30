e2fc938b2fdce9bfabaf1b42f139b49e9d128b6a
derek williams <derek61@gmail.com>
2023-12-30 10:41:59 -0700

    use shadcn card

--- bloglist/bloglist-frontend/src/components/Blog.jsx
+++ bloglist/bloglist-frontend/src/components/Blog.jsx
@@ -1,5 +1,13 @@
 import {useState} from 'react'
 import {Button} from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 const Blog = ({blog, user, incrementLikes, deleteBlog}) => {
   const [showDetails, setShowDetails] = useState(false)
@@ -16,16 +24,17 @@ const Blog = ({blog, user, incrementLikes, deleteBlog}) => {
   }
 
   return (
-    <div className="blog-item" style={blogStyle}>
+    <Card className="blog-item">
+      {/* <div className="blog-item" style={blogStyle}> */}
       {showDetails && (
-        <div>
+        <>
           <div>
-            <span style={itemStyle} className="blog-title">
+            <CardTitle style={itemStyle} className="blog-title">
               {blog.title}
-            </span>
-            <span style={itemStyle} className="blog-author">
+            </CardTitle>
+            <CardTitle style={itemStyle} className="blog-author">
               {blog.author}
-            </span>
+            </CardTitle>
             <Button
               variant="secondary"
               className="hide-blog-details"
@@ -35,10 +44,10 @@ const Blog = ({blog, user, incrementLikes, deleteBlog}) => {
               hide
             </Button>
           </div>
-          <div style={itemStyle} className="blog-url">
+          <CardDescription style={itemStyle} className="blog-url">
             <a href={blog.url}>{blog.url}</a>
-          </div>
-          <div>
+          </CardDescription>
+          <CardFooter>
             <label style={itemStyle} htmlFor="blog-likes">
               likes:
             </label>
@@ -53,8 +62,8 @@ const Blog = ({blog, user, incrementLikes, deleteBlog}) => {
             >
               like
             </Button>
-          </div>
-          <div style={itemStyle}>
+          </CardFooter>
+          <CardFooter>
             {blog &&
               blog.user &&
               user &&
@@ -67,29 +76,74 @@ const Blog = ({blog, user, incrementLikes, deleteBlog}) => {
                 >
                   delete
                 </Button>
-
-                // <button
-                //   data-testid="delete-blog"
-                //   onClick={() => deleteBlog(blog)}
-                // >
-                //   delete
-                // </button>
               )}
-          </div>
-          <div style={itemStyle} className="blog-username">
+          </CardFooter>
+          <CardFooter style={itemStyle} className="blog-username">
             {showDetails && blog.user && blog.user.username}
-          </div>
-        </div>
+          </CardFooter>
+        </>
+
+        // <div>
+        //   <div>
+        //     <span style={itemStyle} className="blog-title">
+        //       {blog.title}
+        //     </span>
+        //     <span style={itemStyle} className="blog-author">
+        //       {blog.author}
+        //     </span>
+        //     <Button
+        //       variant="secondary"
+        //       className="hide-blog-details"
+        //       data-testid="hide-blog-details"
+        //       onClick={() => setShowDetails(false)}
+        //     >
+        //       hide
+        //     </Button>
+        //   </div>
+        //   <div style={itemStyle} className="blog-url">
+        //     <a href={blog.url}>{blog.url}</a>
+        //   </div>
+        //   <div>
+        //     <label style={itemStyle} htmlFor="blog-likes">
+        //       likes:
+        //     </label>
+        //     <span style={itemStyle} className="blog-likes" name="blog-likes">
+        //       {blog.likes}
+        //     </span>
+        //     <Button
+        //       variant="secondary"
+        //       className="increment-blog-like"
+        //       data-testid="increment-blog-like"
+        //       onClick={() => incrementLikes(blog)}
+        //     >
+        //       like
+        //     </Button>
+        //   </div>
+        //   <div style={itemStyle}>
+        //     {blog &&
+        //       blog.user &&
+        //       user &&
+        //       user.username === blog.user.username && (
+        //         <Button
+        //           variant="secondary"
+        //           className="delete-blog"
+        //           data-testid="delete-blog"
+        //           onClick={() => deleteBlog(blog)}
+        //         >
+        //           delete
+        //         </Button>
+        //       )}
+        //   </div>
+        //   <div style={itemStyle} className="blog-username">
+        //     {showDetails && blog.user && blog.user.username}
+        //   </div>
+        // </div>
       )}
       {!showDetails && (
-        <div>
-          <div>
-            <span style={itemStyle} className="blog-title">
-              {blog.title}
-            </span>
-            <span style={itemStyle} className="blog-author">
-              {blog.author}
-            </span>
+        <>
+          <CardHeader>
+            <CardTitle>{blog.title}</CardTitle>
+            <CardDescription>{blog.author}</CardDescription>
             <Button
               variant="secondary"
               className="blog-show-details"
@@ -98,10 +152,27 @@ const Blog = ({blog, user, incrementLikes, deleteBlog}) => {
             >
               show
             </Button>
-          </div>
-        </div>
+          </CardHeader>
+        </>
+        // <div>
+        //   <span style={itemStyle} className="blog-title">
+        //     {blog.title}
+        //   </span>
+        //   <span style={itemStyle} className="blog-author">
+        //     {blog.author}
+        //   </span>
+        //   <Button
+        //     variant="secondary"
+        //     className="blog-show-details"
+        //     data-testid="blog-show-details"
+        //     onClick={() => setShowDetails(true)}
+        //   >
+        //     show
+        //   </Button>
+        // </div>
       )}
-    </div>
+    </Card>
+    // </div>
   )
 }
 
